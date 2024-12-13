a65bff9830d401b8caf5b621347e667457f97542
Mukesh Swain <mukeshswain2844@gmail.com>
2024-12-13 13:52:09 +0530

    check meessage removed

--- BACK-END/src/server.js
+++ BACK-END/src/server.js
@@ -1,36 +1,35 @@
 import express from "express";
-import 'dotenv/config';
-import authRoutes from "./routes/auth.routes.js"
-import cors from "cors"
-import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
-import messageRoutes from "./routes/message.routes.js"
+import "dotenv/config";
+import authRoutes from "./routes/auth.routes.js";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.routes.js";
 import { server, app } from "./lib/socket.io.js";
 import path from "path";
-
 
 const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 // const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../FRONT-END/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../FRONT-END/dist")));
 
-    app.get("*",(req, res) => {
-        res.sendFile(path.join(__dirname, "../FRONT-END","dist","index.html"))
-    })
-    
-}
-  server.listen(port, () => {
-    connectDB();
-    console.log(`Server is running on http://localhost:${port}`);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FRONT-END", "dist", "index.html"));
   });
+}
+server.listen(port, () => {
+  connectDB();
+  console.log(`Server is running on http://localhost:${port}`);
+});
